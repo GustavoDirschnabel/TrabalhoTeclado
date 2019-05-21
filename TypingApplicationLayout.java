9ef0db2f65d2447a9370cdf9008a70f81bcd08b2
JoaoJandre <48719461+JoaoJandre@users.noreply.github.com>
2019-05-21 11:38:02 -0300

    Definidos atributos iniciais

---
+++ TypingApplicationLayout.java
@@ -0,0 +1,16 @@
+package bruh;
+
+import javax.swing.*;
+import java.awt.*;
+public class TypingApplicationLayout extends JFrame {
+	private JTabbedPane tabedPane;
+	private JTextArea textA;
+	private JLabel labelPangrama;
+	private JLabel labelResult;
+	private JButton[] teclado;
+	private final String[] teclas = {"~","1","2","3","4","5","6","7","8","9","0","-","+","Backspace",
+									"Tab","Q","W","E","R","T","Y","U","I","O","P","[","]","'\'",
+									"Caps","A","S","D","F","G","H","J","K","L",":","\"","Enter",
+									"Shift","Z","X","C","V","B","N","M",",",".","?","^",
+									"                                 ","<","v",">"};
+}
