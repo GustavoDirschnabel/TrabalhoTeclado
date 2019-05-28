d4deb0d45f0bcfabfe4698a156abd0a98605ee32
GustavoDirschnabel <48719558+GustavoDirschnabel@users.noreply.github.com>
2019-05-28 10:08:07 -0300

    Mensagens de erro adicionadas
    HistoricoIO agora abre JOptionPanes com os erros que podem ser encontrados durante a execução

--- HistoricoIO.java
+++ HistoricoIO.java
@@ -1,4 +1,5 @@
 
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -6,6 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import javax.swing.JOptionPane;
 
 public class HistoricoIO {
 	private ObjectInputStream input;
@@ -21,16 +23,20 @@ public class HistoricoIO {
 		catch (FileNotFoundException e){
 			try {
 				saveFile.createNewFile();
+				JOptionPane.showMessageDialog(null, "Arquivo de Histórico não encontrado, um novo foi criado","Historico.ser Não Encontrado", JOptionPane.ERROR_MESSAGE);
 			} catch (IOException e1) {
 				System.err.println(e.toString());
+				JOptionPane.showMessageDialog(null, "Erro ao criar um novo arquivo serializado","Historico.ser Não Encontrado", JOptionPane.ERROR_MESSAGE);
 			}
 		}
-		
+		catch (EOFException e) {
+			
+		}
 		catch (IOException e) {
-			System.err.println(e.toString());
+			JOptionPane.showMessageDialog(null, "Arquivo Serializado corrompido ou mal-formado","Erro ao ler Historico.ser", JOptionPane.ERROR_MESSAGE);
 		}
 		catch(ClassNotFoundException e) {
-			System.err.println(e.toString());
+			JOptionPane.showMessageDialog(null, "Não foi possível criar a String durante a leitura","Erro ao ler Historico.ser", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 	
@@ -42,11 +48,14 @@ public class HistoricoIO {
 		catch (FileNotFoundException e){
 			try {
 				saveFile.createNewFile();
+				JOptionPane.showMessageDialog(null, "Um novo arquivo serializado foi criado","Historico.ser Não Encontrado", JOptionPane.ERROR_MESSAGE);
 			} catch (IOException e1) {
 				System.err.println(e.toString());
+				JOptionPane.showMessageDialog(null, "Erro ao criar um novo arquivo serializado","Historico.ser Não Encontrado", JOptionPane.ERROR_MESSAGE);
 			}
 		}
 		catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Arquivo Serializado corrompido ou mal-formado","Erro ao ler Historico.ser", JOptionPane.ERROR_MESSAGE);
 			System.err.println(e.toString());
 		}
 	}
@@ -62,9 +71,13 @@ public class HistoricoIO {
 		openFileOutput();
 		
 		try {
-			output.writeObject(this.historico + newString);
+			if(this.historico != "")
+				output.writeObject(newString + "\n" + this.historico);
+			else
+				output.writeObject(newString);
 		}
 		catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Erro Ao escrever em historico.ser","Erro", JOptionPane.ERROR_MESSAGE);
 			System.err.println(e.toString());
 		}
 		
@@ -81,6 +94,7 @@ public class HistoricoIO {
 		}
 		
 		catch(IOException e) {
+			JOptionPane.showMessageDialog(null, "Erro ao fechar historico.ser","Erro", JOptionPane.ERROR_MESSAGE);
 			System.err.println(e.toString());
 		}
 	}
